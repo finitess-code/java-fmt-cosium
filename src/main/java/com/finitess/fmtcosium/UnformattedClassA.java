ac5e6ff755f0897e10a2675c125b0d333b15ad1a
Oleksandr Shmatko <oleksandr.shmatko@finitess.com>
2024-08-15 12:09:32 +0200

    Initial commit - Maven project & unformatted file

---
+++ src/main/java/com/finitess/fmtcosium/UnformattedClassA.java
@@ -0,0 +1,25 @@
+package com.finitess.fmtcosium;
+
+import java.util.*;
+
+
+public
+class  UnformattedClassA
+{
+
+    void  hello() {
+//        this should return hello
+        System.
+                out
+                .println("hello");
+
+    }
+
+}
+
+class SomeInnerClassA
+
+
+{
+
+}
