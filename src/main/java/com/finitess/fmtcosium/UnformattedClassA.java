c1fadaa8702f40465d04e4a25703ce28d378f15b
Oleksandr Shmatko <oleksandr.shmatko@finitess.com>
2024-08-19 10:46:28 +0200

    Adding more points of formatting validation

--- src/main/java/com/finitess/fmtcosium/UnformattedClassA.java
+++ src/main/java/com/finitess/fmtcosium/UnformattedClassA.java
@@ -1,13 +1,15 @@
 package com.finitess.fmtcosium;
 
 import java.util.*;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.UUID;
 
 
 public
 class  UnformattedClassA
 {
 
-    void  hello() {
+    void        hello() {
 //        this should return hello
         System.
                 out
